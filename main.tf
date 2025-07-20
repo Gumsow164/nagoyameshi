38f44366a4366dd6f57b3c7c679e6e0853a96345
Hiroshi gotou <r.gumsow@icloud.com>
2025-07-21 05:52:02 +0900

    main.tfひな形を作成

---
+++ main.tf
@@ -0,0 +1,30 @@
+#---------------------------------
+# Terraform configuration
+#---------------------------------
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+#---------------------------------
+# Provider
+#---------------------------------
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "terraform"
+}
+
+#---------------------------------
+# Variables
+#---------------------------------
+variable "project" {
+  type    = string
+}
+variable "enviroment" {
+  type    = string
+}
